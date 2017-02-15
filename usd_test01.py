5c7e35b1c61829b29b15bc4aac38acf471d58a17
alphaelf0080 <shau_ren_chiou@hotmail.com>
2017-02-16 01:00:12 +0800

    Revert "usdTest"
    This reverts commit 9c263cb6899d727ac96d09c4a1ce07de11864c71.

--- usd_test01.py
+++
@@ -1,7 +0,0 @@
-import maya.cmds as cmds
-import sys
-sys.path.append("C:/Program Files/Pixar/RenderManProServer-21.2/lib/python2.7/Lib/site-packages")
-sys.path.append("c:/python27")
-sys.path.append("C:/Python27/Lib/site-packages")
-
-impo
